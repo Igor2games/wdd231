2d6a5104d7c504b18dc12c3297151d3c3e08420f
Igor2games <igor1games@outlook.com>
2025-10-27 17:49:04 -0300

    saving

---
+++ scripts/navigation.js
@@ -0,0 +1,7 @@
+/* Create Variable */
+const navbuttom = document.querySelector('#ham-btn');
+
+// toggle show and off
+navbuttom.addEventListener('click', () => {
+    navbuttom.classList.toggle('show');
+});
