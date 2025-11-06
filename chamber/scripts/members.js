a2661596df29e6b90209ae67f68ca7b2b444faf7
Igor2games <igor1games@outlook.com>
2025-11-06 19:03:14 -0300

    member done

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -24,6 +24,7 @@ const displayMembers = (members) => {
         image.setAttribute('loading', 'lazy');
 
         address.textContent = `Address: ${member.address}`;
+        address.setAttribute('class', 'address');
 
         phone.textContent = `Phone: ${member.phone}`;
 
@@ -41,3 +42,22 @@ const displayMembers = (members) => {
 }
 
 getMemberData(src);
+
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
+
+// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
+
+gridbutton.addEventListener("click", () => {
+    // example using arrow function
+    display.classList.add("grid");
+    display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList); // example using defined function
+
+function showList() {
+    display.classList.add("list");
+    display.classList.remove("grid");
+}
