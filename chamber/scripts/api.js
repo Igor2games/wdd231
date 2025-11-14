f62d9d83b086b6b5bb6e791a1de319d1cb9dd119
Igor2games <igor1games@outlook.com>
2025-11-14 13:55:27 -0300

    saving

--- chamber/scripts/api.js
+++ chamber/scripts/api.js
@@ -1,11 +1,10 @@
 const currentTemp = document.querySelector('#temp');
 const weatherIcon = document.querySelector('#weather-icon');
-const captionDesc = document.querySelector('figcaption');
+const captionDesc = document.querySelector('#desc');
 const high = document.querySelector('#high')
 const low = document.querySelector('#low')
 const humidity = document.querySelector('#humidity')
-const sunrise = document.querySelector('#sunrise')
-const sunset = document.querySelector('#sunset')
+
 
 const lat = -21.764064112994504;
 
@@ -31,12 +30,15 @@ async function apiFetch() {
 }
 
 function displayResults(data) {
-    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    currentTemp.innerHTML = `${data.main.temp}&deg;C`;
     const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
     let desc = data.weather[0].description;
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', desc);
     captionDesc.textContent = `${desc}`;
+    high.textContent = `High: ${data.main.temp_max}°C`
+    low.textContent = `Low: ${data.main.temp_min}°C`
+    low.textContent = `Humidity: ${data.main.humidity}%`
 }
 
 apiFetch();
